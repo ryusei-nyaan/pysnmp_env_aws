0db41a27c26e0d18c60c56f094f14cb2d9fafa80
toratti <70882861+ryusei-nyaan@users.noreply.github.com>
2024-03-27 23:37:21 +0900

    Add files via upload

--- snmp_env_create.tf
+++ snmp_env_create.tf
@@ -1,10 +1,14 @@
+# 変数定義
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "key-name" {}
+
 # AWS Provider
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = ""
-  secret_key = ""
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
-
 
 #VPC
 resource "aws_vpc" "test-vpc" {
@@ -13,16 +17,29 @@ resource "aws_vpc" "test-vpc" {
     Name = "test-vpc"
   }
 }
-#サブネット
-resource "aws_subnet" "test-subnet" {
+
+#パブリックサブネット
+resource "aws_subnet" "test-public-subnet" {
   vpc_id                  = aws_vpc.test-vpc.id
   cidr_block              = "172.16.0.0/28"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "test-subnet"
+    Name = "test-public-subnet"
   }
 }
+
+#プライベートサブネット
+resource "aws_subnet" "test-private-subnet" {
+  vpc_id            = aws_vpc.test-vpc.id
+  cidr_block        = "172.16.0.16/28"
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "test-private-subnet"
+  }
+}
+
+
 #インターネットゲートウェイ
 resource "aws_internet_gateway" "test-gateway" {
   vpc_id = aws_vpc.test-vpc.id
@@ -31,27 +48,56 @@ resource "aws_internet_gateway" "test-gateway" {
   }
 }
 #ルートテーブル
-resource "aws_route_table" "test-route-table" {
+resource "aws_route_table" "test-route-table1" {
   vpc_id = aws_vpc.test-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.test-gateway.id
   }
   tags = {
-    Name = "test-route-table"
+    Name = "test-route-table1"
   }
+}
+
+resource "aws_route_table" "test-route-table2" {
+  vpc_id = aws_vpc.test-vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.test-nat.id
+  }
+  tags = {
+    Name = "test-route-table2"
+  }
+}
+
+#NATゲートウェイ
+resource "aws_nat_gateway" "test-nat" {
+  allocation_id = aws_eip.test-eip.id
+  subnet_id     = aws_subnet.test-public-subnet.id
+
+  tags = {
+    Name = "test-nat"
+  }
+
+  depends_on = [aws_internet_gateway.test-gateway]
 }
 
 
 #ルートテーブルとサブネットの紐付け
-resource "aws_route_table_association" "test-association" {
-  subnet_id      = aws_subnet.test-subnet.id
-  route_table_id = aws_route_table.test-route-table.id
+resource "aws_route_table_association" "test-association1" {
+  subnet_id      = aws_subnet.test-public-subnet.id
+  route_table_id = aws_route_table.test-route-table1.id
 }
 
+resource "aws_route_table_association" "test-association2" {
+  subnet_id      = aws_subnet.test-private-subnet.id
+  route_table_id = aws_route_table.test-route-table2.id
+}
+
+
 #セキュリティグループ作成
-resource "aws_security_group" "test-security-group" {
-  name        = "test-security-group"
+resource "aws_security_group" "test-security-group1" {
+  name        = "test-security-group1"
   description = "TEST"
   vpc_id      = aws_vpc.test-vpc.id
 
@@ -60,7 +106,7 @@ resource "aws_security_group" "test-security-group" {
     from_port   = 161
     to_port     = 161
     protocol    = "udp"
-    cidr_blocks = ["172.16.0.0/28"]
+    cidr_blocks = ["172.16.0.16/28"]
   }
 
   ingress {
@@ -77,25 +123,66 @@ resource "aws_security_group" "test-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "test-security-group"
+    Name = "test-security-group1"
+  }
+}
+
+resource "aws_security_group" "test-security-group2" {
+  name        = "test-security-group2"
+  description = "TEST"
+  vpc_id      = aws_vpc.test-vpc.id
+
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "test-security-group2"
   }
 }
 
 
+
+
 #ENI
 resource "aws_network_interface" "test-nw-interface-manager" {
-  subnet_id       = aws_subnet.test-subnet.id
-  private_ips     = ["172.16.0.4"]
-  security_groups = [aws_security_group.test-security-group.id]
+  subnet_id       = aws_subnet.test-private-subnet.id
+  private_ips     = ["172.16.0.20"]
+  security_groups = [aws_security_group.test-security-group2.id]
 
 }
 
 resource "aws_network_interface" "test-nw-interface-agent" {
-  subnet_id       = aws_subnet.test-subnet.id
+  subnet_id       = aws_subnet.test-public-subnet.id
   private_ips     = ["172.16.0.5"]
-  security_groups = [aws_security_group.test-security-group.id]
+  security_groups = [aws_security_group.test-security-group1.id]
 }
 
+#elastic ip
+resource "aws_eip" "test-eip" {
+  domain = "vpc"
+}
+
+
+#EIC
+resource "aws_ec2_instance_connect_endpoint" "manager-eic" {
+  subnet_id = aws_subnet.test-private-subnet.id
+
+  security_group_ids = [
+    aws_security_group.test-security-group2.id,
+  ]
+
+}
 
 
 #EC2
@@ -103,7 +190,7 @@ resource "aws_instance" "test-instance-manager" {
   ami               = "ami-0310b105770df9334"
   instance_type     = "t2.micro"
   availability_zone = "ap-northeast-1a"
-  key_name          = "key-name"
+  key_name          = var.key-name
   network_interface {
     device_index         = 0
     network_interface_id = aws_network_interface.test-nw-interface-manager.id
@@ -131,7 +218,7 @@ resource "aws_instance" "test-instance-agent" {
   ami               = "ami-0310b105770df9334"
   instance_type     = "t2.micro"
   availability_zone = "ap-northeast-1a"
-  key_name          = "key-name"
+  key_name          = var.key-name
 
   network_interface {
     device_index         = 0
